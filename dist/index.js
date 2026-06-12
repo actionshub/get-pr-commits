c0f01e97ed3d353760834c48da4dd39dd4cc799b
Phil Dibowitz <phil@ipom.com>
2026-06-12 14:32:21 -0700

    Fix build harder
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- dist/index.js
+++ dist/index.js
@@ -35468,9 +35468,9 @@ function requireSrc () {
 	    const token = core.getInput('token');
 	    const filterOutPattern = core.getInput('filter_out_pattern');
 	    const filterOutFlags = core.getInput('filter_out_flags');
-	    const octokit = new github.GitHub(token);
+	    const octokit = github.getOctokit(token);
 
-	    const commitsListed = await octokit.pulls.listCommits({
+	    const commitsListed = await octokit.rest.pulls.listCommits({
 	      owner: repo.owner.login,
 	      repo: repo.name,
 	      pull_number: pr.number,
